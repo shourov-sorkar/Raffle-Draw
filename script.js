2b4068c77abb140943552c137d7c2c4cb08b95c0
shourov-sorkar <18103142@iubat.edu>
2021-08-05 03:10:43 +0600

    :tada: Added Functionality

---
+++ script.js
@@ -0,0 +1,88 @@
+window.onload = function () {
+    const inp = document.getElementById('inp')
+    const nameList = document.getElementById('name-list')
+    const display = document.getElementById('display')
+    const giveATry = document.getElementById('give-a-try')
+    const firstPosition = document.getElementById('first-position')
+    const secondPosition = document.getElementById('second-position')
+    const thirdPosition = document.getElementById('third-position')
+
+    const participantNames = []
+
+    inp.addEventListener('keypress', function (event) {
+        if (event.key == 'Enter') {
+            let newNames = event.target.value.split('\n')
+            if (newNames[0] !== '') {
+                newNames.forEach(name => {
+                    participantNames.push(name)
+                    let item = createListItem(name)
+                    nameList.appendChild(item)
+                    event.target.value = ''
+                });
+            }
+        }
+    })
+
+    giveATry.addEventListener('click', function () {
+        if (participantNames.length == 0) {
+            alert('There is No Entry')
+        } else {
+            let shuffledNames = shuffle(participantNames)
+            for (let i = 1; i < shuffledNames.length; i++) {
+                (function (i, count) {
+                    setTimeout(() => {
+                        let rand = Math.floor(Math.random() * (shuffledNames.length))
+                        display.innerHTML = shuffledNames[rand]
+
+                        if (count == shuffledNames.length - 1) {
+                            if (!firstPosition.innerHTML) {
+                                firstPosition.innerHTML = shuffledNames[rand]
+                                let ind = participantNames.indexOf(shuffledNames[rand])
+                                participantNames.splice(ind, 1)
+                            }
+
+                        } else if (!secondPosition.innerHTML) {
+                            secondPosition.innerHTML = shuffledNames[rand]
+                            let ind = participantNames.indexOf(shuffledNames[rand])
+                            participantNames.splice(ind, 1)
+
+
+                        } else if (!thirdPosition.innerHTML) {
+                            thirdPosition.innerHTML = shuffledNames[rand]
+                            let ind = participantNames.indexOf(shuffledNames[rand])
+                            participantNames.splice(ind, 1)
+
+                        }
+                        else {
+                            alert(asdfasdfasf)
+                        }
+
+                    }, i)
+
+                })(i + 100, i)
+            }
+
+        }
+    })
+}
+
+
+function createListItem(name) {
+    let li = document.createElement('li')
+    li.className = 'list-group-item'
+    li.innerHTML = name
+    return li
+}
+
+function shuffle(arr) {
+    let shuffledArr = [...arr]
+
+    for (let i = shuffledArr.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() + (i + 1))
+        let temp = shuffledArr[rand]
+        shuffledArr[rand] = shuffledArr[i]
+        shuffledArr[i] = temp;
+    }
+
+    return shuffledArr
+}
